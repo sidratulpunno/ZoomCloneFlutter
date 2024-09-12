eb2be7faca9dc93f456d8152314ef568e9b9d514
Your Name <sidratulpunno>
2024-09-12 22:48:40 +0600

    added firestore for history of meeting

--- lib/resources/jitsi_meet_methods.dart
+++ lib/resources/jitsi_meet_methods.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
 import 'package:meeting_app/resources/auth_method.dart';
+import 'package:meeting_app/resources/firestore_methods.dart';
 
 class JitsiMeetMethods {
   final AuthMethods _authMethods = AuthMethods();
+  final FireStoreMethods _fireStoreMethods = FireStoreMethods();
 
   void createMeeting({
     required String roomName,
@@ -35,6 +37,7 @@ class JitsiMeetMethods {
       );
 
       // Add this line to join the meeting
+      _fireStoreMethods.addMeetingHistory(roomName);
       jitsiMeet.join(options);
     } catch (error) {
       print("Error: $error");
